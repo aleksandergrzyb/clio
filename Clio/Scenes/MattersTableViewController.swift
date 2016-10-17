d61e3c59f07eb903f36f905fa5f0b1ea69a9c16c
Aleksander <aleksander.grzyb@gmail.com>
2016-10-17 18:57:09 +0200

    add Matters table view controller

---
+++ Clio/Scenes/MattersTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  Created by Aleksander Grzyb on 17/10/16.
+//  Copyright © 2016 Aleksander Grzyb. All rights reserved.
+//
+
+import UIKit
+
+class MattersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
+
+extension MattersTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        return cell
+    }
+}
