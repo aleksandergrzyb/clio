6cb024903d789b31f85cb4a2b3b34caed3878cbf
Aleksander <aleksander.grzyb@gmail.com>
2016-10-17 18:39:48 +0200

    project init

---
+++ Clio/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Aleksander Grzyb on 17/10/16.
+//  Copyright © 2016 Aleksander Grzyb. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ ClioTests/ClioTests.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Aleksander Grzyb on 17/10/16.
+//  Copyright © 2016 Aleksander Grzyb. All rights reserved.
+//
+
+import XCTest
+@testable import Clio
+
+class ClioTests: XCTestCase {
+}
