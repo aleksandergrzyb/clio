5543d419af74b332a04715eaa906841aaed50171
Aleksander <aleksander.grzyb@gmail.com>
2016-10-17 23:40:15 +0200

    add constants file

---
+++ Clio/Supporting Files/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Aleksander Grzyb on 17/10/16.
+//  Copyright © 2016 Aleksander Grzyb. All rights reserved.
+//
+
+import Foundation
+
+struct SegueIdentifier {
+    static let showNotes = "ShowNotes"
+}
+
+struct CellIdentifier {
+    static let matterCell = "MatterCell"
+    static let noteCell = "NoteCell"
+}
