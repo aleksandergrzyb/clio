c092acbf604b1901ba0c5b77d253b5e89423ec57
Aleksander Grzyb <aleksander.grzyb@gmail.com>
2016-10-18 18:13:35 +0200

    create method for creating note

--- Clio/Stores/NotesEndpoint.swift
+++ Clio/Stores/NotesEndpoint.swift
@@ -5,17 +5,42 @@
 
 import Foundation
 
+/// Represents possible errors that could occur when using `Note` resource.
+///
+/// - mappingFailed:    Mapping from JSON to object failed.
+enum NotesResourceError: Error {
+    case mappingFailed
+}
+
 extension Note {
-    /// Creates a resource for all notes.
+    /// Returns all notes for given matter ID.
     static func all(matterId: Int) -> Resource<[Note]> {
-        let params = [ "regarding_type" : "Matter",
+        let params = [ "regarding_type": "Matter",
                        "regarding_id": "\(matterId)" ]
         return Resource(path: "notes", params: params) { json -> Result<[Note]> in
             guard let root = json as? JSONDictionary,
             let dictionaries = root["notes"] as? [JSONDictionary] else {
-                return .failure(MatterResourceError.mappingFailed)
+                return .failure(NotesResourceError.mappingFailed)
             }
             return .success(dictionaries.flatMap(Note.init))
+        }
+    }
+
+    /// Creates note with given data for matter ID.
+    static func createNote(subject: String = "Default subject",
+                           detail: String = "Default detail",
+                           date: String = "2000-01-01",
+                           matterId: Int) -> Resource<Note> {
+        let regarding = [ "type" : "Matter", "id": "\(matterId)" ]
+        let note: JSONDictionary = [ "subject": subject, "detail": detail, "regarding": regarding ]
+        let body: JSONDictionary = [ "note": note ]
+        return Resource(path: "notes", method: .post(body)) { json -> Result<Note> in
+            guard let root = json as? JSONDictionary,
+                let noteDictionary = root["note"] as? JSONDictionary,
+                let note = Note.init(dictionary: noteDictionary) else {
+                return .failure(NotesResourceError.mappingFailed)
+            }
+            return .success(note)
         }
     }
 }
